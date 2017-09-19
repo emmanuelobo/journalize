740d11e45e280f8d82d70ee3591fda285c60c3cf
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-09-19 02:03:13 -0400

    Initial

---
+++ app/journal/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Journal(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=False)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return "%s by %s (%s)" % self.title, self.writer, self.created_at
