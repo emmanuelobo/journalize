3cf72dbddc69bedbb292bb3dd06018f24f0e1794
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-10-17 19:18:48 -0400

    Skeleton Views

--- app/account/views.py
+++ app/account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def profile(request):
+	data = {}
+	return render(request, 'account/profile.html', data)
+
+
+def password_management(request):
+	data = {}
+	return render(request, 'account/passwords.html', data)
+
+
+def email_management(request):
+	data = {}
+	return render(request, 'account/email.html', data)
+
+
+def notifications(request):
+	data = {}
+	return render(request, 'account/notifications.html', data)
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def analytics(request):
+	data = {}
+	return render(request, 'dashboard/analytics.html', data)
+
+
+def data(request):
+	data = {}
+	return render(request, 'dashboard/data.html', data)
+
+
+def summary(request):
+	data = {}
+	return render(request, 'dashboard/summary.html', data)
+
+
+def security(request):
+	data = {}
+	return render(request, 'dashboard/security.html', data)
